6e1e1d761a53449f1e21d157abd521dc2712f8e7
woodmanben <woodmanben01@gmail.com>
2020-10-17 16:36:09 -0400

    cleared useless function

--- imgur.py
+++ imgur.py
@@ -2,21 +2,6 @@
 import configparser
 from imgurpython import ImgurClient
 
-def authenticate():
-    config = configparser.ConfigParser()
-    config.read('auth.ini')
-
-    client_id = config.get('credentials', 'client_id')
-    client_secret = config.get('credentials', 'client_secret')
-
-    imgur_username = config.get('credentials', 'imgur_username')
-    imgur_username = config.get('credentials', 'imgur_password')
-
-    client = ImgurClient(client_id, client_secret)
-
-    authorization_url = client.get_auth_url('pin')
-    print(authorization_url)
-
 config = configparser.ConfigParser()
 config.read('auth.ini')
 

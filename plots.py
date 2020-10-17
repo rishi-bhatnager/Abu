afb89db303f5f0d01f4161081db59dabb09527b5
woodmanben <woodmanben01@gmail.com>
2020-10-17 00:18:35 -0400

    Plots added

--- plots.py
+++ plots.py
@@ -4,15 +4,28 @@ import requests
 import numpy as np
 import matplotlib.pyplot as plt
 
-response = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExposures=true&calculatePerformance=true&positions=AAPL~20")
+response = requests.get("https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculateExpectedReturns=true&calculateExposures=true \
+&calculatePerformance=true&calculateRisk=true&positions=AAPL~150%7CTSLA~50")
 
-returnsMap = response.json()['resultMap']['PORTFOLIOS'][0]['portfolios'][0]['returns']['returnsMap']
+bigData = response.json()['resultMap']['PORTFOLIOS'][0]['portfolios'][0]
+returnsMap = bigData['returns']['returnsMap']
 
-plot_len = 200
-levels = np.ones(plot_len-1)
-lastN = sorted(returnsMap.items())[-plot_len:]
-for i in range(plot_len-1):
-    levels[i] = lastN[i][1]['level']
-plt.plot(levels)
+show_levels = False
+if show_levels:
+    plot_len = 200
+    levels = np.ones(plot_len-1)
+    lastN = sorted(returnsMap.items())[-plot_len:]
+    for i in range(plot_len-1):
+        levels[i] = lastN[i][1]['level']
+    plt.plot(levels)
+    plt.show()
+    print('done')
+
+
+holdings = bigData['holdings']
+portfolio = {}
+for security in holdings:
+    portfolio[security['ticker']] = security['weight']
+
+plt.pie(portfolio.values(),labels=portfolio.keys(),autopct='%1.1f%%')
 plt.show()
-print('done')

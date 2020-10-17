8970f03052f501b92b453fae160b4127941b130e
woodmanben <woodmanben01@gmail.com>
2020-10-17 17:42:38 -0400

    commit

--- securitySearch.py
+++ securitySearch.py
@@ -17,17 +17,19 @@ sector = "technology"
 securities_data = 'https://www.blackrock.com/tools/hackathon/search-securities?datesAsStrings=true&query={}'.format(sector)
 apiSecurity = requests.get(securities_data).json()
 dataS = apiSecurity['resultMap']['SEARCH_RESULTS'][0]['resultList']
-sector_dict = {'tech': ['AAPL','TSLA','SNAP','LYFT']}
-sector = 'tech'
 
+sector = 'Industrials'
 
-def drawSectorPlots(sector_dict, sector):
+def drawSectorPlots(sector):
+    from suggestions import companiesPerSector
+    sector_dict = companiesPerSector()
     #Given sector dictionary and desired sector, create a plot of the overall growth of the sector
     #Based on the values of those securities
     if sector not in sector_dict:
         raise Exception("Sector not in dictionary")
     url = 'https://www.blackrock.com/tools/hackathon/performance?datesAsStrings=true&identifiers='
-    for company in sector_dict[sector]:
+    for company,_ in sector_dict[sector][0:7]:
+        print(company)
         url += company + '%2C'
     url = url[:-3]
     api = requests.get(url).json()
@@ -37,16 +39,16 @@ def drawSectorPlots(sector_dict, sector):
     dates = []
     total_levels = np.zeros(plot_len)
     # Iterate through securities:
-    for i in range(len(sector_dict[sector])):
+    for i in range(len(sector_dict[sector][0:7])):
         daily = data[i]['returnsMap']
         day_list = sorted(daily.items())
         shortened_list = day_list[-plot_len:] # Grab only last year of data
         first_level = shortened_list[0][1]['level']
         for n in range(plot_len):
-            total_levels[n] += shortened_list[n][1]['level']/first_level
+            total_levels[n] += shortened_list[n][1]['level']*sector_dict[sector][i][1]/first_level
     for i in range(plot_len):
         dates.append(dt.datetime.strptime(shortened_list[i][0][0:10],'%Y-%m-%d').date())
-    total_levels = total_levels/3
+    total_levels = total_levels/len(sector_dict[sector])
     plt.plot(dates, total_levels)
     plt.title(sector.upper() + " PERFORMANCE")
     plt.show()
@@ -62,7 +64,6 @@ def drawTickerPlots(years):
     dates = []
     shortened_list = day_list[-plot_len:]
     for i in range(len(shortened_list)-1):
-        print(type(dt.datetime.strptime(shortened_list[i][0][0:10],'%Y-%m-%d').date()))
         dates.append(dt.datetime.strptime(shortened_list[i][0][0:10],'%Y-%m-%d').date())
     for i in range(plot_len - 1):
         levels[i] = shortened_list[i][1]['level']
@@ -72,5 +73,5 @@ def drawTickerPlots(years):
 
 
 if __name__ == '__main__':
-    drawTickerPlots(100)
-    ## drawSectorPlots(sector_dict, 'tech')
+    # drawTickerPlots(100)
+    drawSectorPlots(sector)

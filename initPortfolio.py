0d15429dba14e20c7a03bb91f947e7dc8c7f800d
rishi-bhatnager <rishibhatnager01@gmail.com>
2020-10-17 22:35:42 -0400

    data can be saved

--- initPortfolio.py
+++ initPortfolio.py
@@ -20,3 +20,40 @@ class Portfolio:
 
         self.portAnalCleaned = self.portAnal['resultMap']['PORTFOLIOS'][0]['portfolios'][0]
         self.perfDataCleaned = self.perfData['resultMap']['RETURNS']
+
+
+    def saveData(self, dicts=['portAnalCleaned', 'perfDataCleaned'], folder='./'):
+        '''
+        Saves the given data as a json file into the specified folder
+
+        Params:
+            dicts: string representataions of the names of the dictionaries to save (defaults to the cleaned data from both APIs)
+            folder: the folder to save the json files to (defaults to current directory)
+        '''
+        for var in dicts:
+            if var is 'portAnalCleaned':
+                fileName = 'portAnalCleaned.json'
+                data = self.portAnalCleaned
+
+            elif var is 'perfDataCleaned':
+                fileName = 'perfDataCleaned.json'
+                data = self.perfDataCleaned
+
+            elif var is 'portAnal':
+                fileName = 'portAnal.json'
+                data = self.portAnal
+
+            elif var is 'perfData':
+                fileName = 'perfData.json'
+                data = self.perfData
+
+            else:
+                raise ValueError(f'Passed in invalid argument {var} to saveData().')
+
+            with open(folder+fileName, mode='w') as out:
+                json.dump(data, out)
+
+
+if __name__ == '__main__':
+    test = Portfolio()
+    test.saveData()
